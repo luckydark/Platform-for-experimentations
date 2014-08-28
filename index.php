268a797fdd936fb77ee473612607ea29b9a3951d
akis94 <akispro94@gmail.com>
2014-08-29 00:31:20 +0300

    index.php
    No descriptions.

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang='en'>
+  <head>
+    <title>Турция и Россия</title>
+    <meta>
+    <link rel='stylesheet' href='css/style.css'>
+    <script></script>
+  </head>
+  <body>
+    <header><?php echo '<strong>Header</strong><br>';?></header>
+    <content><?php echo '<h3>Content</h3>'; ?></content>
+    <footer><?php echo '<h4>Footer</h4>'; ?></footer>
+  </body>
+</html>
