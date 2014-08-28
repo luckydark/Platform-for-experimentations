c15cac3f03d857a0e12ac595357a92bd5f5918b3
akis94 <akispro94@gmail.com>
2014-08-29 00:42:22 +0300

    functions.php
    Some functions for a good functional site.

---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php
+
+  // ----- constructors ----- //
+  class ghost{
+    function __construct(){
+    
+    };
+  }
+  class user extends ghost{
+    function __construct(){
+    
+    };
+  }
+  class admin extends user{
+    function __construct(){
+    
+    };
+  }
+  class vip-user extends user{
+    function __construct(){
+    
+    };
+  }
+  
+  //  ----- functions ----- //
+
+?>
